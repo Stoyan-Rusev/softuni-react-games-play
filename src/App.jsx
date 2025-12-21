0bdf7e7b72be89d76ac0c3cfb1435b9f2898bab3
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2025-12-21 19:02:58 +0200

    divide html to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Header from "./components/header/Header"
+import Home from "./components/home/Home"
+
 function App() {
 
   return (
-    <>
+    <div id="box">
+      <Header />
 
-    </>
+      <Home />
+    </div>
   )
 }
 
