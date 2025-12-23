8758829c65f2eb800d2f14bb268ea1595b0fb5cb
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2025-12-23 10:35:35 +0200

    add: react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import { Routes, Route } from 'react-router'
+
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
 
 function App() {
 
-  return (
-    <div id="box">
-      <Header />
+    return (
+        <div id="box">
+            <Header />
 
-      <Home />
-    </div>
-  )
+            <main id="main-content">
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+            </main>
+        </div>
+    )
 }
 
 export default App
