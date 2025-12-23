df6591e3e7dfd2ace61bcc345062a76ff0592986
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2025-12-23 22:56:28 +0200

    feat: create game

--- src/components/create-game/CreateGame.jsx
+++ src/components/create-game/CreateGame.jsx
@@ -1,25 +1,36 @@
+import { create } from "../../services/gameService"; 
+
 export default function CreateGame() {
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const data = Object.fromEntries(formData);
+
+        const newGame = await create(data);
+        console.log(newGame);
+    }
+
     return (
-        <section id="create-page" class="auth">
-            <form id="create">
-                <div class="container">
+        <section id="create-page" className="auth">
+            <form onSubmit={submitHandler} id="create">
+                <div className="container">
 
                     <h1>Create Game</h1>
-                    <label for="leg-title">Legendary title:</label>
+                    <label htmlFor="title">Legendary title:</label>
                     <input type="text" id="title" name="title" placeholder="Enter game title..." />
 
-                    <label for="category">Category:</label>
+                    <label htmlFor="category">Category:</label>
                     <input type="text" id="category" name="category" placeholder="Enter game category..." />
 
-                    <label for="levels">MaxLevel:</label>
+                    <label htmlFor="maxLevel">MaxLevel:</label>
                     <input type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1" />
 
-                    <label for="game-img">Image:</label>
+                    <label htmlFor="imageUrl">Image:</label>
                     <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
 
-                    <label for="summary">Summary:</label>
+                    <label htmlFor="summary">Summary:</label>
                     <textarea name="summary" id="summary"></textarea>
-                    <input class="btn submit" type="submit" value="Create Game" />
+                    <input className="btn submit" type="submit" value="Create Game" />
                 </div>
             </form>
         </section>
