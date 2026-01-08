5b1669dd9ca6ccfe640b9440a1d558803e7405ef
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2026-01-08 22:41:02 +0200

    feat: delete game

--- src/components/details-game/DetailsGame.jsx
+++ src/components/details-game/DetailsGame.jsx
@@ -1,53 +1,77 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router";
+
+import { deleteGame, getOne } from "../../services/gameService";
+ 
 export default function DetailsGame() {
+    const { id } = useParams();
+    const [game, setGame] = useState({});
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        getOne(id)
+            .then(data => setGame(data));
+    }, []);
+
+    const deleteGameHandler = async () => {
+        const hasConfirmed = confirm(`Are you sure you want to delete ${game.title}?`);
+
+        if (!hasConfirmed) return;
+
+        try {
+            await deleteGame(id);
+            navigate('/games');
+        } catch (err) {
+            alert('Failed to delete game: ' + err.message);
+        };
+    };
+
     return (
         <section id="game-details">
             <h1>Game Details</h1>
-            <div class="info-section">
+            <div className="info-section">
 
-                <div class="game-header">
-                    <img class="game-img" src="images/MineCraft.png" />
-                    <h1>Bright</h1>
-                    <span class="levels">MaxLevel: 4</span>
-                    <p class="type">Action, Crime, Fantasy</p>
+                <div className="game-header">
+                    <img className="game-img" src={game.imageUrl} />
+                    <h1>{game.title}</h1>
+                    <span className="levels">MaxLevel: {game.maxLevel}</span>
+                    <p className="type">{game.category}</p>
                 </div>
 
-                <p class="text">
-                    Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work
-                    with an Orc to find a weapon everyone is prepared to kill for. Set in a world where fantasy
-                    creatures live side by side with humans. A human cop is forced
-                    to work with an Orc to find a weapon everyone is prepared to kill for.
+                <p className="text">
+                    {game.summary}
                 </p>
 
                 {/* <!-- Bonus ( for Guests and Users ) --> */}
-                <div class="details-comments">
+                <div className="details-comments">
                     <h2>Comments:</h2>
                     <ul>
                         {/* <!-- list all comments for current game (If any) --> */}
-                        <li class="comment">
+                        <li className="comment">
                             <p>Content: I rate this one quite highly.</p>
                         </li>
-                        <li class="comment">
+                        <li className="comment">
                             <p>Content: The best game.</p>
                         </li>
                     </ul>
                     {/* <!-- Display paragraph: If there are no games in the database --> */}
-                    <p class="no-comment">No comments.</p>
+                    <p className="no-comment">No comments.</p>
                 </div>
 
                 {/* <!-- Edit/Delete buttons ( Only for creator of this game )  --> */}
-                <div class="buttons">
-                    <a href="#" class="button">Edit</a>
-                    <a href="#" class="button">Delete</a>
+                <div className="buttons">
+                    <a href="#" className="button">Edit</a>
+                    <button onClick={deleteGameHandler} className="button">Delete</button>
                 </div>
             </div>
 
             {/* <!-- Bonus --> */}
             {/* <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) --> */}
-            <article class="create-comment">
+            <article className="create-comment">
                 <label>Add new comment:</label>
-                <form class="form">
+                <form className="form">
                     <textarea name="comment" placeholder="Comment......"></textarea>
-                    <input class="btn submit" type="submit" value="Add Comment" />
+                    <input className="btn submit" type="submit" value="Add Comment" />
                 </form>
             </article>
 
--- src/services/gameService.js
+++ src/services/gameService.js
@@ -17,6 +17,24 @@ export const getAll = async() => {
     const response = await fetch(baseUrl);
     const data = await response.json();
     const games = Object.values(data);
-    
+
     return games;
 }
+
+export const getOne = async (gameId) => {
+    const response = await fetch(`${baseUrl}/${gameId}`);
+    const data = await response.json();
+    return data;
+};
+
+export const deleteGame = async (gameId) => {
+    const response = await fetch(`${baseUrl}/${gameId}`, {
+        method: 'DELETE',
+    });
+
+      if (!response.ok) {
+        throw new Error('Failed to delete game');
+    }
+
+    return true;
+};
