d6926b9579ddb8b0e7447737c1794909b48aef4f
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2026-02-05 16:18:07 +0200

    refactor: add server logout

--- src/authApi/useLogout.js
+++ src/authApi/useLogout.js
@@ -1,13 +1,29 @@
 import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
+import { useNavigate } from "react-router";
+
+const baseUrl = 'http://localhost:3030/users/logout';
 
 export default function useLogout() {
-    const { email, setAuthData } = useContext(UserContext);
+    const { accessToken, setAuthData } = useContext(UserContext);
+    const navigate = useNavigate();
 
-    const logout = () => {
-        if (!email) return;
-        setAuthData({});
-        console.log('logged out');
+    const logout = async () => {
+        if (!accessToken) return;
+
+        const response = await fetch(baseUrl, {
+            headers: {
+                'X-Authorization': accessToken,
+            }
+        });
+
+        if (response.status === 204){
+            setAuthData({});
+            navigate('/');
+        } else {
+            alert("Logout failed!");
+            console.error('Logout failed');
+        }
     };
 
     return { logout };
